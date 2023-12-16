f99b189daf4a67a83a9250c89a22217647871b7e
bakr|n.s <126991508+bakr177@users.noreply.github.com>
2023-12-16 21:54:53 +0300

    Add files via upload

---
+++ js/ne.js
@@ -0,0 +1,18 @@
+
+
+    function image() {
+        document.getElementById("ss").src = "image/image 21.png";
+    }
+    function image2() {
+        document.getElementById("ss").src = "image/image 24.png";
+    }
+    function image3() {
+        document.getElementById("ss").src = "image/image 25.png";
+    }
+
+
+    
+    function resetImage() {
+        document.getElementById("ss").src = "image/image 22.png";
+    }
+
